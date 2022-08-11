65d5cee1d0e553a5cde88a7c40c74aa63dec33ab
Aston Junior <Aston Junior@AJ>
2022-08-11 22:13:51 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.yellowlight = New System.Windows.Forms.Label()
+        Me.redlight = New System.Windows.Forms.Label()
+        Me.greenlight = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.greenlight)
+        Me.Panel1.Controls.Add(Me.redlight)
+        Me.Panel1.Controls.Add(Me.yellowlight)
+        Me.Panel1.Location = New System.Drawing.Point(114, 46)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(70, 162)
+        Me.Panel1.TabIndex = 0
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.Black
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(146, 207)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(10, 193)
+        Me.FlowLayoutPanel1.TabIndex = 1
+        '
+        'yellowlight
+        '
+        Me.yellowlight.AutoSize = True
+        Me.yellowlight.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.yellowlight.ForeColor = System.Drawing.Color.Goldenrod
+        Me.yellowlight.Location = New System.Drawing.Point(8, 53)
+        Me.yellowlight.Name = "yellowlight"
+        Me.yellowlight.Size = New System.Drawing.Size(56, 46)
+        Me.yellowlight.TabIndex = 2
+        Me.yellowlight.Text = "⚫"
+        '
+        'redlight
+        '
+        Me.redlight.AutoSize = True
+        Me.redlight.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.redlight.ForeColor = System.Drawing.Color.IndianRed
+        Me.redlight.Location = New System.Drawing.Point(8, 3)
+        Me.redlight.Name = "redlight"
+        Me.redlight.Size = New System.Drawing.Size(56, 46)
+        Me.redlight.TabIndex = 3
+        Me.redlight.Text = "⚫"
+        '
+        'greenlight
+        '
+        Me.greenlight.AutoSize = True
+        Me.greenlight.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.greenlight.ForeColor = System.Drawing.Color.LightGreen
+        Me.greenlight.Location = New System.Drawing.Point(8, 103)
+        Me.greenlight.Name = "greenlight"
+        Me.greenlight.Size = New System.Drawing.Size(56, 46)
+        Me.greenlight.TabIndex = 4
+        Me.greenlight.Text = "⚫"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightGreen
+        Me.Button1.Location = New System.Drawing.Point(213, 321)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "start simulation"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(197, 275)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(161, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Traffic light simualtion"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(472, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents greenlight As Label
+    Friend WithEvents redlight As Label
+    Friend WithEvents yellowlight As Label
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Static time As Integer
+        Select Case time
+            Case 1
+                Timer1.Interval = 2000
+                redlight.ForeColor = Color.Red
+                yellowlight.ForeColor = Color.Goldenrod
+                greenlight.ForeColor = Color.LightGreen
+            Case 2
+                Timer1.Interval = 3000
+                redlight.ForeColor = Color.IndianRed
+                yellowlight.ForeColor = Color.Gold
+                greenlight.ForeColor = Color.LightGreen
+            Case 3
+                Timer1.Interval = 4000
+                redlight.ForeColor = Color.IndianRed
+                yellowlight.ForeColor = Color.Goldenrod
+                greenlight.ForeColor = Color.LimeGreen
+                time = 0
+        End Select
+        time = time + 1
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+End Class
